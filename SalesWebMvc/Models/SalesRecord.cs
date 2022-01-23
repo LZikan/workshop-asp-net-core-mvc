65f61b345f67f773d1d7e9911d999ec7e11b4cf7
Lucas Zikan <lucas.zikan222@gmail.com>
2022-01-23 00:15:27 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Staus { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus staus, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Staus = staus;
+            Seller = seller;
+        }
+    }
+}
